2c9a86e74d98cb7a470f7e9a046a0456c3b0e777
akkoshak <abdulkarim.koshak@gmail.com>
2025-02-06 00:43:47 +0300

    [iOS] Initial commit

---
+++ iOS/finpal/finpal/finpalApp.swift
@@ -0,0 +1,17 @@
+//
+//  finpalApp.swift
+//  finpal
+//
+//  Created by Abdulkarim Koshak on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct finpalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
