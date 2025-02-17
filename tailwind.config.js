0f5067ca76088e9b47a740d17de1afd03d2ac630
MOE ALRUFAIHI <malrufaihi@gmail.com>
2025-02-18 02:44:11 +0300

    enh: Restructered assets, Ar&En json , Global css

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-//tailwind configs
-module.exports = {
-  content: [
-    "./src/**/*.{html,js,ts,jsx,tsx}", // Adjust these to match your actual file types
-    "./index.html"              // Add your main HTML file
-  ],
-  // ... rest of your config
-}
