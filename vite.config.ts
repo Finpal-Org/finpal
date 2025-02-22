a05a18f008b8f7360768774eda8ceeda24db1960
MOE ALRUFAIHI <malrufaihi@gmail.com>
2025-02-22 04:04:44 +0300

    fix: tailwind styles problem, installed through vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   server: {
     port: 3000,
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
+  plugins: [tailwindcss()],
 });
